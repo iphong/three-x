f3cb62b8520c3b2ffb5421260759e887e84f3ad0
Phong Vu <phong.vu@me.com>
2018-07-10 00:40:11 +0700

    Update

---
+++ src/Scene.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+class Scene extends React.Component {
+    render() {
+        return React.createElement("div", null);
+    }
+}
+exports.default = Scene;
+//# sourceMappingURL=Scene.js.map
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-'use strict'
+"use strict";
 function __export(m) {
-	for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p]
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-Object.defineProperty(exports, '__esModule', { value: true })
-__export(require('./Scene'))
-__export(require('./Object'))
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./Scene"));
+__export(require("./Object"));
 //# sourceMappingURL=index.js.map
